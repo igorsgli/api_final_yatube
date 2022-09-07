3c5d5155c6d69589eb0c962754b2903d349f6b89
Igor <li.sg.igor@gmail.com>
2022-09-07 20:46:26 +0300

    6th commit

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -50,7 +50,9 @@ class FollowViewSet(CreateListViewSet):
     search_fields = ('following__username',)
 
     def get_queryset(self):
-        current_user = get_object_or_404(User, username=self.request.user.username)
+        current_user = get_object_or_404(
+            User, username=self.request.user.username
+        )
         return current_user.follower
 
     def perform_create(self, serializer):
